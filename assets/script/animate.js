4aaef7e7bf9835afb2e02874eeaf2edc107e488b
Emily Gosti <egosti@berkeley.edu>
2019-12-28 22:12:09 -0800

    added pic back to home page

--- assets/script/animate.js
+++ assets/script/animate.js
@@ -2,28 +2,34 @@ $(document).ready(function() {
 
   $("#cover-pic, #pic-description").hide();
 
-  $("#hover-prompt").hover(function() {
+  $("#cover-trace").hover(function() {
     $("#cover-pic").fadeIn();
-    $("#cover-pic").animate({
-    	'left': "30%"
-    });
-    $("#cover-pic").animate({
-    	'opacity': "1"
-    });
-    $("#pic-description").delay(800).fadeIn();
-    // $(this).fadeOut();
-    $("#cover-trace").fadeOut();
-    $("#intro").fadeOut();
   }, function() {
-  	$("#pic-description").fadeOut();
-    $("#cover-pic").delay(400).animate({
-    	'left': "20%"
-    });
     $("#cover-pic").fadeOut();
-    // $(this).fadeIn();
-    $("#cover-trace").delay(1000).fadeIn();
-    $("#intro").delay(1000).fadeIn();
   });
+
+  $('#cover-trace').mousemove(function(event){
+    //Both the x and y value are calculated by taking the mouse x,y position on the page and subtracting it from the x,y position of the image on the page. "this" is the hovered element with the class of "img"
+    var mousex = event.pageX - $('#cover-pic').offset().left;
+    var mousey = event.pageY - $('#cover-pic').offset().top;
+    
+    
+    //If you just used the mouse position values the translation effect will only go up and to the right, by subtracting half of the length / width of the imagevfrom the values  we get either a positive or negitive number so that the image will move in any direction.
+    
+    //The 40 controls the amount of "movement" that will happen by giving us a smaller number, feel free to change it to get the effect that you want.
+    var imgx = (mousex - 300) / 30;
+    var imgy = (mousey - 200) / 30;
+    
+    //Adds a translation css styles to the image element
+    $('#cover-pic').css("transform", "translate(" + imgx + "px," + imgy + "px)");
+
+  });
+
+  //This function will fire every time the user mouses off of the image. It resets the translation back to 0.
+  $('#cover-trace').mouseout(function(){
+    $('#cover-pic').css("transform", "translate(0px,0px)");
+  });
+
 
   $("#sidebar-button").click(function() {
     if ($(".sidebar-wrapper").hasClass("sidebar-active") == true) {
